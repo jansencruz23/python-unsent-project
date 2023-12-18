f6902dc2077fecac0adc2f7c188106f9d8908460
Jansen Cruz <103548207+jansencruz23@users.noreply.github.com>
2023-12-18 10:33:37 +0800

    Customize individual record using card and add delete function

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -61,3 +61,14 @@ def customer_record(request, pk):
     else:
         messages.success(request, 'You must be logged in to view a record')
         return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        deleted_it = Record.objects.get(id=pk)
+        deleted_it.delete()
+        messages.success(request, 'Record deleted successfully')
+        return redirect('home')
+    else:
+        messages.success(request, 'You must be logged in to view a record')
+        return redirect('home')
